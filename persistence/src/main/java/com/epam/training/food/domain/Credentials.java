8ac66f627ac92747b8c6c16a5c7e9e7404b63671
jagadish03565 <jagadish03565@gmail.com>
2025-05-02 18:57:28 +0000

    Update Credentials.java

--- persistence/src/main/java/com/epam/training/food/domain/Credentials.java
+++ persistence/src/main/java/com/epam/training/food/domain/Credentials.java
@@ -1,14 +1,55 @@
 package com.epam.training.food.domain;
 
-public class Credentials {
-    private final String username;
-    private final String password;
+import java.util.Objects;
 
-    public Credentials(String username, String password) {
-        this.username = username;
+public class Credentials {
+    private String userName;
+    private String password;
+
+    public Credentials() {
+    }
+
+    public Credentials(String userName, String password) {
+        this.userName = userName;
         this.password = password;
     }
 
-    public String getUsername() { return username; }
-    public String getPassword() { return password; }
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Credentials credentials = (Credentials) o;
+        return Objects.equals(userName, credentials.userName) && Objects.equals(password, credentials.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Credentials{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
