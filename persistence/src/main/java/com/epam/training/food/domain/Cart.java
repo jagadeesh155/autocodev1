9fcece3310c45d8662aa4f98c8f9b57528a63f7a
jagadish03565 <jagadish03565@gmail.com>
2025-05-02 18:56:31 +0000

    Update Cart.java

--- persistence/src/main/java/com/epam/training/food/domain/Cart.java
+++ persistence/src/main/java/com/epam/training/food/domain/Cart.java
@@ -1,26 +1,62 @@
 package com.epam.training.food.domain;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Cart {
-    private final List<OrderItem> items = new ArrayList<>();
+    private List<OrderItem> orderItems;
+    //the field price can be used later to add discounts, so it would differ from the sum of the OrderItems
+    private BigDecimal price;
 
-    public void addItem(OrderItem item) {
-        items.add(item);
+    public Cart() {
     }
 
-    public void removeItem(OrderItem item) {
-        items.remove(item);
+    private Cart(List<OrderItem> orderItems, BigDecimal price) {
+        this.orderItems = orderItems;
+        this.price = price;
     }
 
-    public List<OrderItem> getItems() {
-        return new ArrayList<>(items);
+    //this utility method is not part of the homework specification!
+    public static Cart getEmptyCart() {
+        return new Cart(new ArrayList<>(), BigDecimal.ZERO);
     }
 
-    public double getTotal() {
-        return items.stream()
-                   .mapToDouble(item -> item.getFood().getPrice() * item.getQuantity())
-                   .sum();
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
     }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Cart cart = (Cart) o;
+        return Objects.equals(orderItems, cart.orderItems) && Objects.equals(price, cart.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderItems, price);
+    }
+
+    public void clear() {
+        this.orderItems.clear();
+        this.price = BigDecimal.ZERO;
+    }
+
 }
