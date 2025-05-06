f2a0281980980efb78ab6da512a70891d3ac1ff5
Jagadeesh <jagadeeshmg7@gmail.com>
2025-05-07 00:17:57 +0530

    done

--- Autocode/service/src/main/java/com/epam/training/food/service/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.epam.training.food.service;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
